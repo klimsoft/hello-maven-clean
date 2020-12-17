52412c945ac789f05f7af7bda850924a5f63c2a3
kamil.klimaszewski@gmail.com <kamil.klimaszewski@gmail.com>
2020-12-17 18:13:32 +0100

    Maven project withour wrapper.

---
+++ src/main/java/pl/klimsoft/hello/HelloApplication.java
@@ -0,0 +1,12 @@
+package pl.klimsoft.hello;
+
+
+import java.util.Date;
+
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Maven at " + new Date());
+	}
+
+}
---
+++ src/test/java/pl/klimsoft/hello/HelloApplicationTests.java
@@ -0,0 +1,14 @@
+package pl.klimsoft.hello;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class HelloApplicationTests {
+
+	@Test
+	void sampleTest() {
+		int result = 2 + 2;
+		Assertions.assertEquals(4, 4);
+	}
+
+}
